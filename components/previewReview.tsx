976c38c0d6344f83b1ad44a52c112fe42a9eca97
Igor F. Duca <yelldutz@gmail.com>
2023-12-09 00:17:01 -0300

    fix: escaping game name

--- components/previewReview.tsx
+++ components/previewReview.tsx
@@ -3,7 +3,9 @@ import { Image } from "@nextui-org/react";
 export default function PreviewReview() {
   return (
     <div className="p-4 rounded-lg border border-zinc-800 bg-zinc-950 w-96 h-[31rem]">
-      <h1 className="text-2xl text-indigo-400 font-bold">Baldur's Gate 3</h1>
+      <h1 className="text-2xl text-indigo-400 font-bold">
+        Baldur&apos;s Gate 3
+      </h1>
 
       <div className="grid grid-cols-3 gap-x-2 mt-4">
         <div className="p-1 text-white text-center rounded-lg border-2 border-zinc-800 bg-gradient-to-br from-black via-black to-indigo-950 text-sm">
